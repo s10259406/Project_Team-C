8a31b51aa046a646ac15631a7fbfe7fb7851f938
chaar1ene <s10258547@connect.np.edu.sg>
2023-08-09 16:23:12 +0800

    oo

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 import csv
-fp =Path.home()/
-# with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-#     reader = csv.reader(file)
-#     next(reader)
-
-#     cashonhands = []
-
-#     for row in reader:
-#         cashonhands.append([row[0],row[1]])
-
+fp =Path.home()/"p4b igp"/"Project_Team-C"/"csv_reports"/"Cash_on_Hand.csv"
+# print(fp.exists())
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    next(reader)
+    cashonhands = []
+    for row in reader:
+        cashonhands.append([row[0],row[1]])
 # print(cashonhands)
