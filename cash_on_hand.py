0143208be9be1dd51b687f972a1044b7f86202e4
chaar1ene <s10258547@connect.np.edu.sg>
2023-08-10 01:15:39 +0800

    ll

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -13,19 +13,22 @@ prev_coh = 0
 highest_surplus = 0
 for item in cashonhands:
     coh = int(item[1])
+    # checking whether there is a surplus and calculating it
     if coh < prev_coh:
         surplus = prev_coh - coh
         print(f'[CASH SURPLUS] DAY: {item[0]}, AMOUNT: USD{surplus}')
+        # finding out highest surplus among all the other surpluses
         if surplus > highest_surplus:
             highest_day = f"{item[0]}"
             highest_surplus = surplus
     prev_coh = coh
 print(f"[HIGHEST CASH SURPLUS] DAY: {highest_day}, AMOUNT: USD{highest_surplus}")
 
+
 for item in cashonhands:
     coh = int(item[1])
+    # checking whether there is a deficit and calculating it
     if coh > prev_coh:
         deficit = coh - prev_coh
         print(f'[CASH DEFICIT] DAY: {item[0]}, AMOUNT: USD{deficit}')
     prev_coh = coh
-
