b7f35092978c84b03361b0aa1284a693e6f0f3ec
chaar1ene <s10258547@connect.np.edu.sg>
2023-08-10 01:47:33 +0800

    l

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import csv
-fp =Path.home()/"p4b igp"/"Project_Team-C"/"csv_reports"/"Cash_on_Hand.csv"
+fp = Path.cwd()/"csv_reports"/"Cash_on_Hand.csv"
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
