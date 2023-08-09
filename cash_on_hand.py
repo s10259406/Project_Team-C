09797942c7920290fd5a32ef069f94c002df1608
chaar1ene <s10258547@connect.np.edu.sg>
2023-08-09 16:14:37 +0800

    oo

---
+++ cash_on_hand.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+import csv
+fp =Path.home()/
+# with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+#     reader = csv.reader(file)
+#     next(reader)
+
+#     cashonhands = []
+
+#     for row in reader:
+#         cashonhands.append([row[0],row[1]])
+
+# print(cashonhands)
