fba8017dea4c3a7706ec9c8bcfb5dbcbb40bdab3
s10259406 <s10259406@connect.np.edu.sg>
2023-08-10 21:46:58 +0800

    try

--- main.py
+++ main.py
@@ -1,2 +1,32 @@
-with open('paymentSummary.txt', 'w') as file:
-    file.write()
+# import your modules
+import overheads, cash_on_hand, profit_loss
+
+def main(option):
+
+    '''
+    - The function will return the highest overhead, surplus and deficit of both cash on hands & profit and loss
+    - Required parameter: option
+    '''
+    
+    # open the file in write mode
+    with open('summary_report.txt', 'w') as file:
+        
+        if option == 'SURPLUS':
+            # write some output to the file
+            file.write('Scenario with SURPLUS\n')
+            # call the module functions and write the results to the file
+            file.write(f'{overheads.overheads_function("max_ovh")}')
+            file.write(f'{cash_on_hand.coh_function("Surplus")}')
+            file.write(f'{profit_loss.profitloss_function("Surplus")}')
+
+        elif option == 'DEFICIT':
+            # write some output to the file
+            file.write('Scenario with DEFICIT\n')
+            # call your module functions and write the results to the file
+            file.write(f'{overheads.overheads_function("max_ovh")}')
+            file.write(f'{cash_on_hand.coh_function("Deficit")}')
+            file.write(f'{profit_loss.profitloss_function("Deficit")}')
+
+# call main function with an option
+main('SURPLUS')
+# main('DEFICIT')
