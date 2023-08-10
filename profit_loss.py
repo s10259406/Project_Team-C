e9d7488590856368421e350d15009a998d5ca4c4
s10259406 <s10259406@connect.np.edu.sg>
2023-08-10 21:42:32 +0800

    try

--- profit_loss.py
+++ profit_loss.py
@@ -4,7 +4,7 @@ import csv
 # create a file to csv file.
 fp = Path.cwd()/"csv_reports"/"Profit_and_Loss.csv"
 
-# read the csv file to append profit and quantity from the csv.
+# create 'reader' object and print line if file path exists
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader) # skip header
@@ -13,48 +13,61 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     profitnloss = []
     # append day and net profit into profitnloss list
     for row in reader:
-        net_profit = float(row[4])
+        net_profit = int(row[4])
         # get the day and net profit for each record and append the profitnloss list
         profitnloss.append([row[0],net_profit])
 # print(profitnloss)
 
-profit_deficit = 0   
-day_profit_deficit = 0
-max_profit_surplus = 0
-day_profit_surplus = 0
+def profitloss_function (option):
+   
+  '''
+  - The function will return the profit surplus and profit deficit
+  - Required parameter: option
+  '''
 
- # Use a loop to iterate over the profitnloss list (*the start in range is default to 0)
-for data in range(len(profitnloss)):
+  # initialize variables to store the data
+  previous_net_profit = 0
+  max_profit_surplus = 0
+  day_profit_surplus = 0
 
-    # get the current day and net profit
-    day = profitnloss[data][0]
-    net_profit = profitnloss[data][1]
+  # initialize an empty string to store the results
+  results = ''
 
-    # if it is the 1st day, there is no previous day to compare with
-    if data == 0: # day = [0][0] = day 1 
-      # thus the difference is zero
-      difference = 0
-    else:
-      # get the previous day's net profit
-      previous_net_profit = profitnloss[data-1][1]
-      # calculate the difference by subtracting the previous net profit from the current net profit
-      difference = net_profit - previous_net_profit
+  if option == 'Surplus':
+      # use a for loop to iterate over the data in profitnloss list
+      for data in profitnloss:
+        day = data[0]
+        net_profit = int(data[1])
 
-      # check if the difference is negative, mean there is a deficit
-      if difference < 0:
-        profit_deficit = difference * -1
-        day_profit_deficit = day 
-        print(f'[PROFIT DEFICIT] Day: {day_profit_deficit}, Amount: USD{profit_deficit}')
-      
-      # check if the difference is positive, mean there is a surplus
-      elif difference > 0:
-        profit_deficit = difference
-        day_profit_deficit = day 
-        print(f'[PROFIT SURPLUS] Day: {day_profit_deficit}, Amount: USD{profit_deficit}')
+        # checking if the net profit on the current day is higher than the previous day
+        if net_profit > previous_net_profit:
+           # calculate the difference by subtracting the previous net profit from the current net profit
+           difference = net_profit - previous_net_profit
+           results += f'[NET PROFIT SURPLUS] DAY:{day}, AMOUNT: USD{difference}\n'
+           # finding the highest profit surplus among all the profit surpluses
+           if difference > max_profit_surplus:
+              day_profit_surplus = data[0]
+              max_profit_surplus = difference
+        previous_net_profit = net_profit
+      results += f'[HIGHEST PROFIT SURPLUS] DAY:{day_profit_surplus}, AMOUNT: USD{max_profit_surplus}\n'
+      return results
 
-      # compare the difference with the max_profit_deficit
-      if difference > 0:
-        if difference > max_profit_surplus:
-          max_profit_surplus = difference
-          day_profit_surplus = day     
-print(f'[HIGHEST PROFIT SURPLUS] Day: {day_profit_surplus}, Amount: USD{max_profit_surplus}')
+
+  elif option == 'Deficit':
+     #  use a for loop to iterate over the data in profitnloss list
+     for data in profitnloss:
+        day = data[0]
+        net_profit = int(data[1])
+
+        # checking if the net profit on the current day is lower than the previous day
+        if net_profit < previous_net_profit:
+           # calculate the difference by subtracting the previous net profit from the current net profit
+           difference = (net_profit - previous_net_profit) * -1
+           results += f'[PROFIT DEFICIT] DAY:{day}, AMOUNT: USD{difference}\n'
+        previous_net_profit = net_profit
+  return results
+
+# print(profitloss_function('Surplus'))
+# print(profitloss_function('Deficit'))
+
+           
